8c8f2c2ed02ccb1b54c5b6329474c697b20a0b04
Steven Chavez <schavez256@yahoo.com>
2026-09-15 12:20:32 -0600

    Create 002_Comments.c

---
+++ C/Fundamentals/002_Comments.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+// Single-line comments uses "//" at the beginning
+int main() { // This is also a single-line comment
+  /* 
+  This is a multi-line 
+  comment.
+  */
+  
+  printf("Hello World!");
+  // You can also use comments to disable code. Mainly for troubleshooting. 
+  //printf("Hello World!"); <-- This printf() statement won't run
+  
+  return 0; 
+}
