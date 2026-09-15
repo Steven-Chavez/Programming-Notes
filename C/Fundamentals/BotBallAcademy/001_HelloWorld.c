5fcd598378010c33b9c46c606f04b7e3d295869b
Steven Chavez <schavez256@yahoo.com>
2026-09-15 12:27:51 -0600

    Create 001_HelloWorld.c

---
+++ C/Fundamentals/BotBallAcademy/001_HelloWorld.c
@@ -0,0 +1,7 @@
+#include <kipr/wombat.h>  // this is the file with all the kipr functions
+#include <stdio.h>        // this is the file that allows for output
+ 
+int main(){
+     printf("Hello World!\n");
+     return 0;
+}
