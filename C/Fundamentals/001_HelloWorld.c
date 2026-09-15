5d96f5223c83d3d3a43500770b7cdbb4cdb34ea2
Steven Chavez <schavez256@yahoo.com>
2026-09-15 07:51:20 -0600

    Update 001_HelloWorld.c

--- C/Fundamentals/001_HelloWorld.c
+++ C/Fundamentals/001_HelloWorld.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+//tells C to include a header file. This header lets us use input/output functions such as printf()
+#include <stdio.h> 
 
-int main() {
+// is a special function. Your program starts running here. Any code inside the curly brackets {} will be executed.
+int main() { 
+  // printf() is a function used to output (print) text to the screen. In our example, it prints Hello World!.
   printf("Hello World!");
-  return 0;
+  
+  //ends the main() function and sends a value back to the operating system.
+  return 0; 
 }
