132cc831fc47dd97552fd9436afbfd6444badf4c
Steven Chavez <schavez256@yahoo.com>
2026-09-24 08:18:18 -0600

    Update jbc_4.c

--- C/BotBallAcademy/Challenges/jbc_4.c
+++ C/BotBallAcademy/Challenges/jbc_4.c
@@ -1,6 +1,6 @@
 // Author: Steven Chavez
 // Date: 9/24/2026
-// Challenge: JBC 3 (Optimized Arc Motion)
+// Challenge: JBC 4
 #include <kipr/wombat.h>
 #include <stdio.h>
 
@@ -18,27 +18,29 @@ int main() {
 	
 	// Can 1
 	drive(50, 20, 3500);
-	drive(20, 50, 5000);
+	drive(15, 50, 4000);
+	drive(-50, 50, 2000);
 
 	// Can 2
-	drive(-20, 50, 2000);
-	drive(40, 50, 2000);
+	drive(50, 50, 1000);
 
 	// Can 3
-	drive(50, 40, 1500);
-	drive(50, -20, 5000);
+	drive(50, 10, 7000);
 
 	// Can 4
-	drive(50, 50, 3000);
-	
-	// Can 5
-	drive(0, 50, 6500);
+	drive(60, -30, 2000);
 
-	// Can 6 & 7
-	drive(50, 50, 3000);
+	// Can 5
+	drive(10, 50, 6000);
+
+	// Can 6
+	drive(-30, 50, 2000);
+
+	// Can 7
+	drive(50, 30, 3500);
 
 	// Can 8
-	drive(50, 0, 5000);
+	drive(50, 0, 4000);
 	
 	ao(); // Turn off all motors at the end
 	return 0;
